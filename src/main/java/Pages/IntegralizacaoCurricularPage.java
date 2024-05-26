df44fa10e03563f1c2993526e30446e2c7b944c3
CaiqueBohlhalter <cbohlhalter@live.com>
2024-05-26 20:13:36 -0300

    Page increment

---
+++ src/main/java/Pages/IntegralizacaoCurricularPage.java
@@ -0,0 +1,41 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class IntegralizacaoCurricularPage {
+    private static final String PAGE_TITLE = ":: UNESP : Câmpus de Rio Claro ::";
+    private static final String PAGE_URL = "https://sistemas.unesp.br/academico/aluno/cadastro.integralizacaoCurricular.action";
+    private static final String DEFAULT_TITLE = "Integralização Curricular";
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @FindBy(xpath = "//*[@id=\"ct\"]/h2")
+    WebElement title;
+
+    public IntegralizacaoCurricularPage(WebDriver driver){
+        this.driver = driver;
+        driver.get(PAGE_URL);
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    private String getPageTitle(){
+        return driver.getTitle();
+    }
+
+    public Boolean isCorrectPage(){
+        wait.until(ExpectedConditions.titleContains(PAGE_TITLE));
+        if(getPageTitle().equals(PAGE_TITLE)){
+            return title.getText().contains(DEFAULT_TITLE);
+        }
+        return false;
+    }
+}
