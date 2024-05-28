9eb832c933065106bafa55bf771dfe3729cab867
CaiqueBohlhalter <cbohlhalter@live.com>
2024-05-27 22:07:58 -0300

    MensagensEnviadasPageTest

--- src/main/java/pages/graduationSystem/actionPages/MensagensEnviadasPage.java
+++ src/main/java/pages/graduationSystem/actionPages/MensagensEnviadasPage.java
@@ -13,12 +13,22 @@ public class MensagensEnviadasPage {
     private static final String PAGE_TITLE = ":: UNESP : Câmpus de Rio Claro ::";
     private static final String PAGE_URL = "https://sistemas.unesp.br/academico/mensagem.listar.action?emailTipo=enviadas";
     private static final String DEFAULT_TITLE = "Mensagens enviadas";
+    private static final String INFO_MSG = "Visualizar todas mensagens";
 
     WebDriver driver;
     WebDriverWait wait;
 
     @FindBy(xpath = "//*[@id=\"ct\"]/h2")
     WebElement title;
+
+    @FindBy(xpath = "//*[@id=\"mensagem\"]")
+    WebElement sendMessagesTable;
+
+    @FindBy(xpath = "//*[@id=\"form_mensagens\"]/div/span")
+    WebElement registersCount;
+
+    @FindBy(xpath = "//*[@id=\"cc\"]/div")
+    WebElement infoMsg;
 
     public MensagensEnviadasPage(WebDriver driver){
         this.driver = driver;
@@ -37,6 +47,18 @@ public class MensagensEnviadasPage {
             return title.getText().contains(DEFAULT_TITLE);
         }
         return false;
+    }
+
+    public Boolean isCorrectIdentificationShown() {
+        return infoMsg.getText().contains(INFO_MSG);
+    }
+
+    public Boolean isSendMessagesTableShown() {
+        return sendMessagesTable.isDisplayed();
+    }
+
+    public Boolean isRegistersCountDisplayed() {
+        return registersCount.isDisplayed();
     }
 
 }
